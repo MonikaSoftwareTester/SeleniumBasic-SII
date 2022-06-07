ddb3471f24fbaae05d16085b6f2da0724ba44e7c
Monika Wieczorek <monika.wieczorek.88@gmail.com>
2022-06-07 23:21:03 +0200

    Initial commit

---
+++ src/test/java/homework/DatePicker.java
@@ -0,0 +1,24 @@
+package homework;
+
+import base.TestBase;
+import ch3_TestBase.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DatePicker extends TestBase {
+
+    @Test
+    public void chooseCorrectDate(){
+
+        driver.get("https://seleniumui.moderntester.pl/datepicker.php");
+
+        WebElement datePicker = driver.findElement(By.xpath("//input[@type='text']"));
+
+        datePicker.sendKeys("31/08/2022");
+
+        Assert.assertEquals(datePicker.getAttribute("value"), "31/08/2022");
+
+    }
+}
